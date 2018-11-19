d18f7a367140d861188eccc4ff04d0765e33e141
YunyiShen <yshen99@outlook.com>
2018-11-18 22:27:50 -0600

    Continue Laplacian Approximation of  logE

--- R/ZIGAM.R
+++ R/ZIGAM.R
@@ -33,7 +33,7 @@ post.weight_helper = function(n,det.vec, lambda,p.vec,psi,N){
 
 ## RSZIGAM.dis.R
 
-RSZIGAM.dis <- function(formula, formula.det ,maxiter = 20, conv.crit = 1e-3,
+RSZIGAM.pois <- function(formula, formula.det ,maxiter = 20, conv.crit = 1e-3,
                       size = NULL, data=list(), N,...) # data should contains detmat as det.1,det.2,det.3 etc, period should be detection period number data's formate: data$detmat should be a matrix with nrow = n.site, ncol = nperiod, data$envX, which is the second should be the environmental data at each site, the else, say data$detX.1 data$detX.2 should be the detection varible at all sites and time period 1, 2...etc., colnames should be consistent
 {
   
@@ -52,10 +52,10 @@ RSZIGAM.dis <- function(formula, formula.det ,maxiter = 20, conv.crit = 1e-3,
   d.f0 <- function(mu) -exp(-mu)
   den <- dpois; disp <- 1; est.disp <- FALSE
   size <- rep.int(1, n.site)
-  loglikfun <- function(y, mu, p) {
-      e <- as.numeric(y!=0)
-      sum((1-e)*log(1-p+p*dpois(y,mu,log=FALSE))+e*(log(p)+dpois(y,mu,log=TRUE)))# likelihood with zero inflated
-    }
+  # loglikfun <- function(y, mu, p) {
+      # e <- as.numeric(y!=0)
+      # sum((1-e)*log(1-p+p*dpois(y,mu,log=FALSE))+e*(log(p)+dpois(y,mu,log=TRUE)))# likelihood with zero inflated
+    # }
 
   
   variance <- family$variance
@@ -81,9 +81,9 @@ RSZIGAM.dis <- function(formula, formula.det ,maxiter = 20, conv.crit = 1e-3,
   ## restart here 11/14/2018 22:20 give up MATH632
   
   lambda <- pmax(apply(data$detmat,1,mean), 5) # Poisson lambda
-  # psi <- rep(0.7, n.site) # occupancy psi
-  psi = runif(n.site)
-  p.vec = ( 0.1*(data$detmat>=0)) # detction p
+  psi <- rep(0.7, n.site) # occupancy psi
+  # psi = runif(n.site)
+  p.vec = ( 0.1*(data$detmat>=0)) # detection p initial value global 0.1
   p = matrix((p.vec),nrow = n.site*period,ncol = 1)
   # quasi.psi = matrix(runif(n.site)>0.5,nrow = n.site,ncol=1)
   quasi.lambda = pmax(apply(data$detmat,1,mean), 0.01) 
@@ -100,9 +100,9 @@ RSZIGAM.dis <- function(formula, formula.det ,maxiter = 20, conv.crit = 1e-3,
   
   wg.lambda = matrix(0,n.site,1)
   wg.p = wg.lambda
-  while( norm > conv.crit & repli < maxiter) { # this is the EM-PIRLS process hopefully 
+  while( norm > conv.crit & repli < maxiter) { # this is the EM-PIRLS process
     
-  quasi.y = data$detmat
+    quasi.y = data$detmat # make quasi.y the matrix form
 	for(i in 1:n.site){ # again, to get quasi data of occupancy status, which is just posterior probability given all parameters
 	    nvec = max(data$detmat[i,]):N
 		# quasi data for occupancy status
@@ -131,16 +131,16 @@ RSZIGAM.dis <- function(formula, formula.det ,maxiter = 20, conv.crit = 1e-3,
 	
 	G.lambda = gam(fm.lambda,family = quasipoisson, fit = FALSE, data=data.frame(quasi.psi,data$envX),...)
 	G.lambda$w = wg.lambda # change the weight in this iter, weight for the data is actually psi, see eq.9a in the technical report, here the weight is set before, see document E-step
-  G.det = gam(fm.p,family = quasibinomial, fit=FALSE, data=data.frame(quasi.y,detdata[,-1]),...)
-  G.det$w = rep(wg.p,period)
+    G.det = gam(fm.p,family = quasibinomial, fit=FALSE, data=data.frame(quasi.y,detdata[,-1]),...)
+    G.det$w = rep(wg.p,period)
 	fit.psi <- gam(G = G.psi) # PIRLS
-  fit.lambda <- gam(G = G.lambda)
+    fit.lambda <- gam(G = G.lambda)
 	fit.p = gam(G = G.det)
-  beta.psi <- coef(fit.psi)
-  beta.lambda = coef(fit.lambda)
+	beta.psi <- coef(fit.psi)
+	beta.lambda = coef(fit.lambda)
 	beta.p = coef(fit.p)
     
-  lambda.old <- lambda
+	lambda.old <- lambda
 	p.old <- p
 	psi.old = psi
 	
@@ -255,38 +255,46 @@ RSZIGAM.dis <- function(formula, formula.det ,maxiter = 20, conv.crit = 1e-3,
   X.p = G.p$X
   
   loglik <- log_likelihood(data$detmat,lambda,p.vec,psi,N) # log-likelihood
-  ploglik <- loglik - as.numeric(0.5*t(b.psi)%*%Lambda.psi%*%b.psi) -  as.numeric(0.5*t(b.lambda)%*%Lambda.lambda%*%b.lambda) - as.numeric(0.5*t(b.p)%*%Lambda.p%*%b.p)
+  ploglik <- loglik - as.numeric(0.5*t(psi)%*%Lambda.psi%*%psi) -  as.numeric(0.5*t(lambda)%*%Lambda.lambda%*%lambda) - as.numeric(0.5*t(p)%*%Lambda.p%*%p)
   
   # stop here 13:33 11/17/2018
   # Model selection criterion
-  I.theta <- matrix(0, ncol=np.psi+np.lambda+np.p, nrow=np.psi+np.lambda+np.p)
-  tau.mu <- -size
-  rho.p <- rep.int(-1, n)
-  a <- 1-p+p*den(0, mu)
-  tau.mu[y==0] <- ( -p*mu.eta.val/disp/variance(mu)/a*(den(0,mu)-size*mu*den(0,mu)*
-                                                         (variance(mu)*d.eta.mu(mu)+d.V(mu)/mu.eta.val)*mu.eta.val/variance(mu)+size*(1-p)*mu*d.f0(mu)/a) )[y==0]
-  rho.p[y==0] <- ( (1-den(0,mu))/(a^2)*(-a*(1-2*p)-(1-den(0,mu))*p*(1-p)) )[y==0]
+  I.theta <- matrix(0, ncol=np.psi+np.lambda+np.p, nrow=np.psi+np.lambda+np.p)  # neg Hessian at MPLE, COZIGAM has a good approximation using Laplace method to approximate the logE, including a term use this, we can derive this analytically 
+  # this matrix will be block diag matrix with block to be Hessian of psi, Hessian of lambda and Hessian of p 
+  tau.lambda <- -size
+  rho.psi <- rep.int(-1, n.site)
+  rho.p = rep.int(-1,n.site*period)
+  # Below is the approximation of Hessian 
+  # Hessian block to lp of psi, no penalty yet
+  Hessian.lp.psi = matrix(0,ncol = np.psi,nrow = np.psi)
+  # Hessian block to lp of lambda
+  Hessian.lp.lambda = matrix(0,ncol = np.lambda,nrow = np.lambda)
+  # Hessian block to lp of p
+  Hessian.lp.p = matrix(0,ncol = np.p,nrow = np.p)
   
-  G.tau.mu <- diag(as.vector(mu.eta.val*tau.mu))
-  G.rho.p <- diag(as.vector(p*(1-p)*rho.p))
+  # add penalty 
+  I.theta[1:np.psi,1:np.psi] = Hessian.lp.psi - Lambda.psi
+  I.theta[(np.psi+1):(np.psi+np.lambda),(np.psi+1):(np.psi+np.lambda)] = Hessian.lp.lambda - Lambda.lambda
+  I.theta[(np.psi+np.lambda+1):(np.p+np.psi+np.lambda),(np.psi+np.lambda+1):(np.p+np.psi+np.lambda)] = Hessian.lp.p - Lambda.p
   
-  I.theta[1:np1,1:np1] <- t(X1) %*% G.tau.mu %*% X1 - Lambda1
-  I.theta[(np1+1):(np1+np2),(np1+1):(np1+np2)] <- t(X2) %*% G.rho.p %*% X2 - Lambda.lambda
-  I.theta <- -I.theta
+  I.theta = -I.theta # this is the negative Hessian matrix, 
   
-  logE <- 0.5*determinant(DS1)$modulus + 0.5*determinant(DS2)$modulus+ploglik +
-    (np1+np2-(nrow(DS1)+nrow(DS2)))/2*log(2*pi)-0.5*determinant(I.theta)$modulus
+  logE = 0.5*determinant(DS.psi)$modulus + 0.5*determinant(DS.lambda)$modulus+ploglik + 0.5*determinant(DS.p)$modulus + 
+    (np.psi+np.lambda + np.p - (nrow(DS.psi) + nrow(DS.lambda) + nrow(DS.p)))/2*log(2*pi)-0.5*determinant(I.theta)$modulus
   attr(logE, "logarithm") <- NULL
   
-  V.theta <- solve(I.theta)
-  V.beta <- V.theta[1:np1, 1:np1]
-  V.gamma <- V.theta[(np1+1):(np1+np2),(np1+1):(np1+np2)]
+  V.theta = solve(I.theta)
+  V.beta.psi = V.theta[1:np.psi,1:np.psi]
+  V.beta.lambda = V.theta[(np.psi+1):(np.psi+np.lambda),(np.psi+1):(np.psi+np.lambda)]
+  V.beta.p = V.theta[(np.psi+np.lambda+1):(np.p+np.psi+np.lambda),(np.psi+np.lambda+1):(np.p+np.psi+np.lambda)]
   
-  fit.gam$Vp <- V.beta; fit.lr$Vp <- V.gamma
+  fit.psi$Vp = V.beta.psi
+  fit.lambda$Vp = V.beta.lambda
+  fit.p$Vp = V.beta.p
   
-  res <- list(formula=formula, logE=logE, ploglik=ploglik, loglik=loglik, fit.gam=fit.gam, fit.lr=fit.lr,
-              mu=mu, p=p, psi=psi, dispersion=disp, V.beta=V.beta, V.gamma=V.gamma, X1=X1, X2=X2, family=family)
-  res
+  res = list(formula.psi=formula, formula.lambda = formula, formula.p = formula.det ,logE=logE, ploglik=ploglik, loglik=loglik,  fit.models = list(fit.psi=fit.psi, fit.lambda=fit.lambda,fit.p = fit.p),fit.values = list(psi=psi, lambda = lambda, p=p), V.beta = list( V.beta.psi=V.beta.psi, V.beta.lambda=V.beta.lambda,V.beta.p=V.beta.p), X=list(X.psi,X.lambda,X.p))
+  class(res) = "RSZIGAM.Poisson.result"
+  return(res)
   
 }
 
